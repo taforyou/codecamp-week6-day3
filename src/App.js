c8d853301a2dd9d47a6a103c373a5fad2174efe3
taforyou <taforyou@hotmail.com>
2018-01-31 10:07:47 +0700

    Edit App.js
    ลบทิ้ง 2 Components เพื่อเริ่มวันใหม่

--- src/App.js
+++ src/App.js
@@ -11,8 +11,6 @@ class App extends Component {
     return (
         <div className="App">
           <Todo />
-          <Todo myColor="green"/>
-          <Todo myColor="red"/>
         </div>
     );
   }
